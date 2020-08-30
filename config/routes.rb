ff2aa463bee1ca4ca611ce9ff580ef462b855db9
jun <gum5588@gmail.com>
2020-08-30 20:38:13 +0900

    add: pagination

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'videos#index'
+  # root to: 'videos#index'
 
   namespace :api do
     resources :books, only: [:show]
@@ -12,5 +12,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
+  # Book一覧取得用のパス
+  get '/api/videos', to: 'api/videos#index'
+
+  # Book一覧表示用のパス
+  get '/videos', to: 'videos#index'
 
 end
