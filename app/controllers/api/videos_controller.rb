e4a8245cabd3f085eab40b8dbf969972df1d6e14
jun <gum5588@gmail.com>
2020-09-01 04:09:53 +0900

    fix: pagenation

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,26 +1,9 @@
 class Api::VideosController < ApplicationController
-
   def index
-  	# 表示するページの番号を指定
-    page = params[:page] || 1
-    # １ページあたりの表示件数を指定
-    per = params[:per] || 20
-    # ページネーションで指定レコードを取得
-    videos = Video.all.page(page).per(per)
-    # ページネーションした時の全ページ数
-    total_pages = videos.total_pages
-    # レスポンスデータの定義
-    response = {
-      # videoレコード
-      videos: videos.select(:view, :title, :video_url, :order),
-      total_pages: total_pages
+    videos = Video.all
+    rows = {
+      videos: videos.select(:view, :title, :video_url, :order)
     }
-    # json形式でレスポンスを返却
-    render json: response
-    puts response
+    render json: rows
   end
-  # def show
-  #   @video = Video.find(params[:id])
-  #   render 'show', formats: 'json', handlers: 'jbuilder'
-  # end
 end
